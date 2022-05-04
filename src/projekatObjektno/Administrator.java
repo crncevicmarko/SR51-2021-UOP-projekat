e886891b71621c0b6cbb95777ed8e364dc395912
crncevicmarko <crncevic.sr51.2021@uns.ac.rs>
2022-05-04 22:22:38 +0200

    Provera komita

--- src/projekatObjektno/Administrator.java
+++ src/projekatObjektno/Administrator.java
@@ -4,7 +4,6 @@ public class Administrator extends Zaposleni {
 
 	public Administrator() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public Administrator(String korisnickoIme, String korisnickaSifra) {
